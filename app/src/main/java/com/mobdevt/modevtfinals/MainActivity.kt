f2b05c25502fcfb4b5e669a7fd6a06c62157a297
Gerard Gabriel Parinas <gerardgabriel.parinas@benilde.edu.ph>
2022-08-08 18:32:37 +0700

    Added Easy

--- app/src/main/java/com/mobdevt/modevtfinals/MainActivity.kt
+++ app/src/main/java/com/mobdevt/modevtfinals/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mobdevt.modevtfinals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdevt.modevtfinals.databinding.ActivityMainBinding
+import com.mobdevt.modevtfinals.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+
+        }
     }
 }
