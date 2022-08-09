97e66aac5a77bccd4cd431266ef6a2f5ff914d7a
George Encarnacione <georgeangelo.encarnacion@benilde.edu.ph>
2022-08-09 13:33:00 +0800

    started with difficulty and main activity

--- app/src/main/java/com/mobdevt/modevtfinals/DifficultyActivity.kt
+++ app/src/main/java/com/mobdevt/modevtfinals/DifficultyActivity.kt
@@ -1,11 +1,38 @@
 package com.mobdevt.modevtfinals
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.HardwarePropertiesManager
+import androidx.appcompat.app.AppCompatActivity
+import com.mobdevt.modevtfinals.databinding.ActivityDifficultyBinding
+
 
 class DifficultyActivity : AppCompatActivity() {
+
+
+    private val btnEasy by lazy {binding.btnEasy}
+    private val btnMedium by lazy {binding.btnMedium}
+    private val btnHard by lazy {binding.btnHard}
+
+
+
+    private lateinit var binding: ActivityDifficultyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_difficulty)
+        binding = ActivityDifficultyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        btnEasy.setOnClickListener {
+            //val intent = Intent(this, EasyActivity::class.java)
+            startActivity(intent)
+        }
+        btnMedium.setOnClickListener {
+            val intent = Intent(this, MediumActivity::class.java)
+            startActivity(intent)
+        }
+        btnHard.setOnClickListener {
+            //val intent = Intent(this, HardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/mobdevt/modevtfinals/MediumActivity.kt
+++ app/src/main/java/com/mobdevt/modevtfinals/MediumActivity.kt
@@ -2,10 +2,19 @@ package com.mobdevt.modevtfinals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdevt.modevtfinals.databinding.ActivityMainBinding
 
 class MediumActivity : AppCompatActivity() {
+
+    //private val tvQuestion by lazy {binding.tvQuestion}
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_medium)
-    }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        }
+
 }
