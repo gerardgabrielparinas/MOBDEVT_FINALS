e795dc2a60c996c87e33ab056adad3c9ba78e89e
JemsIzHere <james.maranion13@gmail.com>
2022-08-11 20:25:28 +0800

    Big UI Changes (Home, Splash, Difficulty), To fix: Rotation Layout and Other UI Elements

--- app/src/main/java/com/mobdevt/modevtfinals/MainActivity.kt
+++ app/src/main/java/com/mobdevt/modevtfinals/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mobdevt.modevtfinals
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.mobdevt.modevtfinals.databinding.ActivityMainBinding
 import com.mobdevt.modevtfinals.MainViewModel
 
@@ -10,16 +12,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
-        binding.btnStart.setOnClickListener {
-            val intent = Intent(this, DifficultyActivity::class.java)
 
-            intent.putExtra("name", "Name")
-
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
-        }
+            finish()
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)//fade page
+        },5000)
     }
 }
